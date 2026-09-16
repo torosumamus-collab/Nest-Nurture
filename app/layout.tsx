0cd27d3750625a43ce47f29deba5674ae25aa3d4
torosumamus-collab <torosumamus@gmail.com>
2026-09-16 06:01:59 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,8 +54,8 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.svg",
   },
- other: {
-    "p:domain_verify": "3ef8725a30c07ece477b70d579f8203a",
+verification: {
+    google: "iqqvUjmqc8vn113mwNowW0N4StlorUxrilVGAWl1NAg",
   },
 };
 
